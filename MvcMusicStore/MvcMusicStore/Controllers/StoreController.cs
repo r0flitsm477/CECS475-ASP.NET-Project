988be7c41d8458abf65a3c7f2ca9631924c05a51
Matthew Kim <matthew.kim64@gmail.com>
2017-12-07 12:44:27 -0800

    page 53

--- MvcMusicStore/MvcMusicStore/Controllers/StoreController.cs
+++ MvcMusicStore/MvcMusicStore/Controllers/StoreController.cs
@@ -21,14 +21,15 @@ namespace MvcMusicStore.Controllers
         // GET: /Store/Browse
         public ActionResult Browse(string genre)
         {
-            var genreModel = new Genre { Name = genre };
+            // Retrieve Genre and its Associated Albums from database
+            var genreModel = storeDB.Genres.Include("Albums").Single(g => g.Name == genre);
             return View(genreModel);
         }
 
         // GET: /Store/Details
         public ActionResult Details(int id)
         {
-            var album = new Album { Title = "Album " + id };
+            var album = storeDB.Albums.Find(id);
             return View(album);
         }
     }
