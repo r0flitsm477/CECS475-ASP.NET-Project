5f94702f2e4be59bc456db4a2fa35b97c291df1b
Matthew Kim <matthew.kim64@gmail.com>
2017-12-07 19:31:01 -0800

    db account register and delete

---
+++ MvcMusicStore/MvcMusicStore/Controllers/AccountController.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MvcMusicStore.Models;
+
+namespace MvcMusicStore.Controllers
+{
+    public class AccountController : Controller
+    {
+        private AccountDbContext db = new AccountDbContext();
+
+        // GET: Account
+        public ActionResult Index()
+        {
+            using (db)
+            {
+                return View(db.userAccount.ToList());
+            }
+        }
+
+        // GET: /Account/Register
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        // POST: /Account/Register
+        [HttpPost]
+        public ActionResult Register(UserAccount account)
+        {
+            if (ModelState.IsValid)
+            {
+                using (AccountDbContext db = new AccountDbContext())
+                {
+                    db.userAccount.Add(account);
+                    db.SaveChanges();
+                }
+                ModelState.Clear();
+                ViewBag.Message = account.Username + " successfully registered!";
+            }
+            return View();
+        }
+
+        // GET: /Account/Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        // POST: /Account/Login
+        [HttpPost]
+        public ActionResult Login(UserAccount user)
+        {
+            using (AccountDbContext db = new AccountDbContext())
+            {
+                var usr = db.userAccount.Where(u => u.Username == user.Username && u.Password == user.Password).FirstOrDefault();
+                if (usr != null)
+                {
+                    Session["UserID"] = usr.UserID.ToString();
+                    Session["Username"] = usr.Username.ToString();
+                    return RedirectToAction("LoggedIn");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Username or Password is wrong.");
+                }
+            }
+            return View();
+        }
+        
+        public ActionResult LoggedIn()
+        {
+            if (Session["UserID"] != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+        }
+        
+        // Delete
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserAccount usr = db.userAccount.Find(id);
+            if (usr == null)
+            {
+                return HttpNotFound();
+            }
+            return View(usr);
+        }
+        
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            UserAccount usr = db.userAccount.Find(id);
+            db.userAccount.Remove(usr);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
