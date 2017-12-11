9a113e2e8ba46302dd9f90e09156bdeb14f30ee4
Matthew Kim <matthew.kim64@gmail.com>
2017-12-11 00:22:59 -0800

    idk some updates
    remove from cart still does not work

--- MvcMusicStore/MvcMusicStore/Controllers/ShoppingCartController.cs
+++ MvcMusicStore/MvcMusicStore/Controllers/ShoppingCartController.cs
@@ -12,10 +12,12 @@ namespace MvcMusicStore.Controllers
     {
         MusicStoreEntities storeDB = new MusicStoreEntities();
 
+        //
         // GET: /ShoppingCart/
         public ActionResult Index()
         {
             var cart = ShoppingCart.GetCart(this.HttpContext);
+
             // Set up our ViewModel
             var viewModel = new ShoppingCartViewModel
             {
@@ -26,19 +28,24 @@ namespace MvcMusicStore.Controllers
             return View(viewModel);
         }
 
+        //
         // GET: /Store/AddToCart/5
         public ActionResult AddToCart(int id)
         {
             // Retrieve the album from the database
             var addedAlbum = storeDB.Albums
-            .Single(album => album.AlbumId == id);
+                .Single(album => album.AlbumId == id);
+
             // Add it to the shopping cart
             var cart = ShoppingCart.GetCart(this.HttpContext);
+
             cart.AddToCart(addedAlbum);
+
             // Go back to the main store page for more shopping
             return RedirectToAction("Index");
         }
 
+        //
         // AJAX: /ShoppingCart/RemoveFromCart/5
         [HttpPost]
         public ActionResult RemoveFromCart(int id)
@@ -57,7 +64,7 @@ namespace MvcMusicStore.Controllers
             var results = new ShoppingCartRemoveViewModel
             {
                 Message = Server.HtmlEncode(albumName) +
-                " has been removed from your shopping cart.",
+                    " has been removed from your shopping cart.",
                 CartTotal = cart.GetTotal(),
                 CartCount = cart.GetCount(),
                 ItemCount = itemCount,
@@ -72,6 +79,7 @@ namespace MvcMusicStore.Controllers
         public ActionResult CartSummary()
         {
             var cart = ShoppingCart.GetCart(this.HttpContext);
+
             ViewData["CartCount"] = cart.GetCount();
             return PartialView("CartSummary");
         }
